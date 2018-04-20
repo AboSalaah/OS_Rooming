6de65d0032aa7c35df5f2cf6eb64b6ccdfb6e70b
AboSalaah <ahmed_saleh102010@yahoo.com>
2018-04-20 08:19:58 +0200

    getting the available rooms

--- public/app.js
+++ public/app.js
@@ -1,26 +1,79 @@
+var content = [];
+
 $( "#guest").click(function() {
     $(".container").remove();
-    $('body').append('<div id="grid"></div>');
-    var content = [];		
-		content[0] = '<div class="element nonmetal"><p class="number">1</p><h3 class="symbol">H</h3><h2 class="name">Hydrogen</h2><p class="weight">1.00794</p></div>';
-        content[1] = '<div class="element nonmetal"><p class="number">1</p><h3 class="symbol">H</h3><h2 class="name">Hydrogen</h2><p class="weight">1.00794</p></div>';
-        content[2] = '<div class="element nonmetal"><p class="number">1</p><h3 class="symbol">H</h3><h2 class="name">Hydrogen</h2><p class="weight">1.00794</p></div>';
-        content[3] = '<div class="element nonmetal"><p class="number">1</p><h3 class="symbol">H</h3><h2 class="name">Hydrogen</h2><p class="weight">1.00794</p></div>';
-        $(function(){
-            $('#grid').jresponsive({
-                min_size: 50,
-                max_size: 200,
-                hspace: 50,
-                vspace: 10,
-                height:	200,
-                class_name: 'element',
-                content_array: content,
-                transfromation: 'animate'
-            });
-            console.log($('#grid').width());
+    // request to get the rooms
+    $.getJSON('/rooms', function(data){ 
+     alert("success");
+     console.log(data);
+     $('body').append('<div id="grid"></div>');
+      for (var i = 0; i < data['rooms'].length; i++) {
+        var room = data['rooms'][i];
+        console.log(room.name);
+        content.push('<div class="element room"><h3 class="room_name">'+room.name+'</h3> <h6 class="room_good">'+room.good+'</h6><h6 class="room_bad">'+room.bad+'</h6><button type="button" id="good_button">Good</button><button type="button" id="bad_button">Bad</button></div>');
+   }
+   
+    console.log(content);
+    $(function(){
+        $('#grid').jresponsive({
+            min_size: 50,
+            max_size: 200,
+            hspace: 50,
+            vspace: 10,
+            height:	200,
+            class_name: 'element',
+            content_array: content,
+            transfromation: 'animate'
         });
+        console.log($('#grid').width());
+    });
 
-     // $.get(data ely 3awzha,)
 
+    var myrooms= $("#grid div");
+    var good_button=$("#good_button");
+    var bad_button=$("#bad_button");
+    good_button.on("click",function(){
+         var good_num = parseInt(good_button.siblings('.room_good'));
+
+         var bad_num = parseInt(good_button.siblings('.room_bad'));
+         var room_name = good_button.siblings('.room_name')
+         var data={
+            name : room_name,
+            good : good_num+1,
+            bad : bad_num
+         };
+          User_Connection(data);
+    });
+    bad_button.on("click",function(){
+         var good_num =  parseInt(bad_button.siblings('.room_good'));
+         var bad_num =   parseInt(bad_button.siblings('.room_bad'));
+         var room_name = bad_button.siblings('.room_name')
+         var data={
+            name : room_name,
+            good : good_num,
+            bad : bad_num+1
+         };
+         User_Connection(data);
+    });
+    function User_Connection(data){
+        var socket=io.connect('http://localhost:3000');
+        socket.emit('room', data);
+         $("#grid").remove();
+    }
+    /*myrooms.on("click",function(){
+    var room_name = $(this).find('.room_name').text();
+    var socket=io.connect('http://localhost:3000');
+    var data={
+        room: room_name
+    }
+    socket.emit('room', data);
+    $("#grid").remove();
+
+    
+    });
+    */
+    
+  });
 
   });
+  
--- server.js
+++ server.js
@@ -1,45 +1,83 @@
 'use strict';
+class Room {
+  constructor(name, good,bad) {
+    this.name = name;
+    this.good = good;
+    this.bad=bad;
+  }
+  // Getter
+  get roomName() {
+    return this.name;
+  }
+  get goodNumbers(){
+    return this.good;
+  }
+  get badNumbers(){
+    return this.bad;
+  }
+}
+var rooms_map=new Map();
+let rooms_players={
+  players : []
+}
+
+var myMap = new Map()
+const system_room_number=5;
 var express = require('express');
 var app=express();
-var server =app.listen(3000);
-var myMap = new Map()
+var server = app.listen(3000,function(){
+  console.log("inti rooms now");
+  for(let i =1;i<=system_room_number;i++){
+    rooms_map.set("SystemRoom: "+i,new Room(("Systemroom: "+i),0,0));
+ }
+   console.log(rooms_map);
+});
 app.use(express.static('public'));
 var socket = require('socket.io');
 var io=socket(server);
 io.sockets.on('connection',newConnection);
 function newConnection(socket){
-    console.log("user: "+socket.id);
-    socket.on('room', function(data) {
+  console.log("user: "+socket.id);
+  socket.on('room', function(data) {
 
-      //  console.log("data.rooom : " + data.room);
-        socket.join(data.room,function(){
-            //  console.log(myMap);
-            //  console.log("Hiiiiiiiiiii");
-              var arr = myMap[data.room];
-              var m = new Map(arr);
-              var xs = Array.from(m.keys());
-              var ys = Array.from(m.values());
-              for (var i = 0; i < xs.length; i++) {
-              //  console.log("Helloooo : " + socket.id);
-                socket.emit('mouse', {x: xs[i], y: ys[i] , room:data.room});
-              }
-          //    console.log(xs);
-          //    console.log(ys);
+    //  console.log("data.rooom : " + data.room);
+      socket.join(data.room,function(){
+        console.log("data.room : "+data.room);
+          rooms_players[data.room]= [];
+          socket.id= rooms_players[data.room].length;
+          console.log(rooms_players[data.room].length);
+          rooms_players[data.room][socket.id]=socket;
+          console.log(rooms_players[data.room]);   
+      });
 
-        });
+      console.log("users in room: "+data.room);
+      var room = io.sockets.adapter.rooms[data.room];
+      console.log(room.length);
+  });
 
-        console.log("users in room: "+data.room);
-        var room = io.sockets.adapter.rooms[data.room];
-        console.log(room.length);
-    });
+ 
 
-    socket.on('mouse',function(data){
-
-      // this works as expected: all clients receive event except the sender
-        socket.broadcast.to(data.room).emit('mouse',data);
-        myMap[data["room"]] = myMap[data["room"]] || [];
-        myMap[data["room"]].push([data["x"],data["y"]]);
-      //  console.log(myMap);
-       //socket.broadcast.emit('mouse',data);
-    });
 }
+
+app.get('/rooms',function(req,response){
+  console.log("Room handler was called.");
+  response.writeHead(200, {"Content-Type": "application/json"});
+  var temp =[];
+  var iterator1 = rooms_map[Symbol.iterator]();
+
+  for (const [key, value] of rooms_map) {
+    console.log(`${key}: ${value}`);
+
+     temp.push(value);
+    }
+   console.log(temp);
+  var json = JSON.stringify({ 
+    rooms : temp
+  });
+  response.end(json);
+});
+app.get('/', function(req, res){
+  console.log(res);
+});
+
+
